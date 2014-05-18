62d09a381ef185c2179473d68c1f3e29cd218f74
elgoog <elgoog.elibom@gmail.com>
2014-05-18 10:56:42 +0900

    add the answer of bonus drink.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,16 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    my_bottle = amount
+    empty_bottle = 0
+    while my_bottle != 0
+      my_bottle -= 1
+      empty_bottle += 1
+      my_bottle += 1 if empty_bottle % 3 == 0
+    end
+    empty_bottle
   end
+end
+
+if $PROGRAM_NAME == __FILE__
+  puts BonusDrink.total_count_for(100)
 end
